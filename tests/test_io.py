1ffae858c7e885fd874eed0bf94fa51b0e97bb65
Leonid Kostrykin <void@evoid.de>
2024-09-24 21:23:25 +0200

    Fix #4

--- tests/test_io.py
+++ tests/test_io.py
@@ -12,3 +12,14 @@ class imread(unittest.TestCase):
     def test_input2(self):
         img = giatools.io.imread('tests/data/input2.tif')
         self.assertEqual(img.mean(), 9.543921821305842)
+
+    def test_input3(self):
+        """
+        This is a multi-page TIFF, that sometimes fails to load properly with ``skimage.io.imread``, but works with
+        ``tifffile``.
+
+        For details see: https://github.com/BMCV/galaxy-image-analysis/pull/132#issuecomment-2371561435
+        """
+        img = giatools.io.imread('tests/data/input3.tif')
+        self.assertEqual(img.shape, (5, 198, 356))
+        self.assertEqual(img.mean(), 1259.6755334241288)
