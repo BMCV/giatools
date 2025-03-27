a78742127c9eddc582d44899a75210cc22a04e58
Leonid Kostrykin <void@evoid.de>
2025-03-27 10:57:27 +0100

    Update __init__.py

--- giatools/__init__.py
+++ giatools/__init__.py
@@ -2,6 +2,6 @@ from .image import Image  # noqa: F401
 
 VERSION_MAJOR = 0
 VERSION_MINOR = 3
-VERSION_PATCH = 3
+VERSION_PATCH = 2
 
 VERSION = '%d.%d%s' % (VERSION_MAJOR, VERSION_MINOR, '.%d' % VERSION_PATCH if VERSION_PATCH > 0 else '')
