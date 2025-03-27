cd044cd8032c9bc3edd18907658c65f000634b23
Leonid Kostrykin <void@evoid.de>
2025-03-27 09:56:04 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
 project = 'giatools'
 copyright = '2017-2025 Leonid Kostrykin, Biomedical Computer Vision Group, Heidelberg University'
 author = 'Leonid Kostrykin'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
