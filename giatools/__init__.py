a937b9260dc0728ccb0f1bee5b44ebcb66e933a3
Leonid Kostrykin <void@evoid.de>
2025-12-09 13:50:21 +0100

    Fix linting

--- giatools/__init__.py
+++ giatools/__init__.py
@@ -5,8 +5,8 @@ Distributed under the MIT license.
 See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
 """
 
-from .image import (
-    Image,  # noqa: F401
-    default_normalized_axes  # noqa: F401
+from .image import (  # noqa: F401
+    Image,
+    default_normalized_axes
 )
 from .version import __version__ as VERSION  # noqa: F401
