2a7b201f4772b4d572ce145257bac5c41db3dce6
Leonid Kostrykin <void@evoid.de>
2025-12-09 13:29:47 +0100

    Add copyright header

--- giatools/__init__.py
+++ giatools/__init__.py
@@ -1,3 +1,10 @@
+"""
+Copyright 2017-2025 Leonid Kostrykin, Biomedical Computer Vision Group, Heidelberg University.
+
+Distributed under the MIT license.
+See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+"""
+
 from .image import (
     Image,  # noqa: F401
     default_normalized_axes  # noqa: F401
