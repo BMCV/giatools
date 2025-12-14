392535f28f19e88f894428e27ac8ba56f2e2422d
Leonid Kostrykin <void@evoid.de>
2025-12-14 11:23:49 +0100

    wip

--- giatools/image.py
+++ giatools/image.py
@@ -1,4 +1,4 @@
-f#rom __future__ import annotations  # properly format TypeAliases in Sphinx
+#from __future__ import annotations  # properly format TypeAliases in Sphinx
 
 """
 Copyright 2017-2025 Leonid Kostrykin, Biomedical Computer Vision Group, Heidelberg University.
