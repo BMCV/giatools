0c0f21b3396415e9966649f7271fe1c68e71619c
Leonid Kostrykin <void@evoid.de>
2025-12-14 01:03:03 +0100

    Add test for `ValueError` in `Image.write` and update docstring

--- giatools/image.py
+++ giatools/image.py
@@ -95,6 +95,9 @@ class Image:
     ) -> Self:
         """
         Write the image to a file.
+
+        Raises:
+            ValueError: If the number of axes does not match the number of data dimensions.
         """
         if len(self.axes) != len(self.data.shape):
             raise ValueError(
