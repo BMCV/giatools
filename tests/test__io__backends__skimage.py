9793a2a16ca75eff17e1708d9a8ba97bcbead2f0
Leonid Kostrykin <void@evoid.de>
2025-12-16 08:27:10 +0100

    Add tests for `SKImageReader`

--- tests/test__io__backends__skimage.py
+++ tests/test__io__backends__skimage.py
@@ -120,3 +120,22 @@ class SKImageWriter__write(unittest.TestCase):
         self.writer.write(array, filepath=filename, metadata=dict(axes='YXC'), quality=90)
         mock_imsave.assert_called()
         self.assertEqual(mock_imsave.call_args.kwargs['quality'], 90)
+
+
+class SKImageReader(unittest.TestCase):
+
+    def test__valid(self):
+        with giatools.io._backends.skimage.SKImageReader('tests/data/input4_uint8.png') as reader:
+            self.assertEqual(reader.get_num_images(), 1)
+            im = reader.select_image(0)
+            self.assertEqual(reader.get_axes(im), 'YXC')
+            self.assertEqual(reader.get_image_metadata(im), dict())
+            arr = reader.get_image_data(im)
+            self.assertEqual(arr.shape, (10, 10, 3))
+            self.assertEqual(round(arr.mean(), 2), 130.04)
+
+    def test__invalid(self):
+        with giatools.io._backends.skimage.SKImageReader('tests/data/input7_uint8_zcyx.tif') as reader:
+            self.assertEqual(reader.get_num_images(), 1)
+            with self.assertRaises(giatools.io.UnsupportedFileError):
+                reader.select_image(0)
