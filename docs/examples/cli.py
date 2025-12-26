abc1f08c726164adb810a60d762714c03c0cd1c0
Leonid Kostrykin <kostrykin@evoid.de>
2025-12-26 23:51:45 +0100

    Employ `.. runblock:: console`

--- docs/examples/cli.py
+++ docs/examples/cli.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     tool.add_input_image('input')
     tool.add_output_image('output')
     for section in tool.run('ZYX'):
-        arr = section['input'].data
-        section['output'] = (arr > arr.mean())
+        section['output'] = (
+            section['input'].data > section['input'].data.mean()
+        )
--- giatools/cli.py
+++ giatools/cli.py
@@ -17,16 +17,9 @@ class ToolBaseplate:
 
         .. literalinclude :: examples/cli.py
 
-        .. runblock:: pycon
+        .. runblock:: console
 
-            >>> import giatools
-            >>> if __name__ == '__main__':
-            ...     tool = giatools.ToolBaseplate(params_required=False)
-            ...     tool.add_input_image('input')
-            ...     tool.add_output_image('output')
-            ...     for section in tool.run('ZYX'):
-            ...         arr = section['input'].data
-            ...         section['output'] = (arr > arr.mean())
+            $ python -m examples.cli --help
     """
 
     input_keys: _T.List[str]
