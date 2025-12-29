645affc3b8674b145a392f9ed5a945d2902bf138
Leonid Kostrykin <void@evoid.de>
2025-12-29 10:00:14 +0100

    Add integration tests for `output_dtype_hints`

--- tests/integration/test__image_processor.py
+++ tests/integration/test__image_processor.py
@@ -146,3 +146,107 @@ class ImageProcessor__process(ImageProcessorTestCase):
         for result in ('result1', 'result2'):
             self.assertEqual(processor.outputs[result].axes, self.images[0].axes)
             self.assertEqual(processor.outputs[result].metadata, self.images[0].metadata)
+
+
+class ImageProcessor__process__output_dtype_hint(ImageProcessorTestCase):
+
+    image_filepaths = [
+        'tests/data/input4_uint8.png',
+    ]
+
+    def setUp(self):
+        super().setUp()
+        self.expected_result = np.stack(
+            [
+                self.images[0].data[..., c] > self.images[0].data[..., c].mean()
+                for c in range(self.images[0].data.shape[-1])
+            ],
+            axis=-1,
+        )
+
+    @minimum_python_version(3, 11)
+    @permute_axes('YX', name='joint_axes')
+    def test__yx__binary(self, joint_axes):
+        processor = giatools.image_processor.ImageProcessor(self.images[0])
+        for section in processor.process(joint_axes, output_dtype_hints={'result': 'binary'}):
+            section['result'] = (section[0].data > section[0].data.mean())
+        np.testing.assert_array_equal(
+            processor.outputs['result'].data,
+            self.expected_result.astype(np.uint8) * 255,
+        )
+
+    @minimum_python_version(3, 11)
+    @permute_axes('YX', name='joint_axes')
+    def test__yx__bool(self, joint_axes):
+        processor = giatools.image_processor.ImageProcessor(self.images[0])
+        for section in processor.process(joint_axes, output_dtype_hints={'result': 'bool'}):
+            section['result'] = ((section[0].data > section[0].data.mean()) * 10).astype(np.int8) - 20
+        np.testing.assert_array_equal(
+            processor.outputs['result'].data,
+            self.expected_result,
+        )
+
+    @minimum_python_version(3, 11)
+    @permute_axes('YX', name='joint_axes')
+    def test__yx__exact_float(self, joint_axes):
+        for dtype_str in ('float16', 'float32', 'float64'):
+            with self.subTest(dtype_str=dtype_str):
+                processor = giatools.image_processor.ImageProcessor(self.images[0])
+                for section in processor.process(joint_axes, output_dtype_hints={'result': dtype_str}):
+                    section['result'] = (section[0].data > section[0].data.mean())
+                np.testing.assert_array_equal(
+                    processor.outputs['result'].data,
+                    self.expected_result.astype(getattr(np, dtype_str)),
+                )
+
+    @minimum_python_version(3, 11)
+    @permute_axes('YX', name='joint_axes')
+    def test__yx__floating(self, joint_axes):
+        for dtype_str in ('float16', 'uint8', 'bool'):
+            with self.subTest(dtype_str=dtype_str):
+                processor = giatools.image_processor.ImageProcessor(self.images[0])
+                for section in processor.process(joint_axes, output_dtype_hints={'result': 'floating'}):
+                    section['result'] = (section[0].data > section[0].data.mean()).astype(getattr(np, dtype_str))
+                np.testing.assert_array_equal(
+                    processor.outputs['result'].data,
+                    self.expected_result.astype(
+                        np.float16 if dtype_str == 'float16' else np.float64,
+                    ),
+                )
+
+    @minimum_python_version(3, 11)
+    @permute_axes('YX', name='joint_axes')
+    def test__yx__preserve(self, joint_axes):
+        assert self.images[0].data.dtype == np.uint8  # sanity check
+        processor = giatools.image_processor.ImageProcessor(self.images[0])
+        for section in processor.process(joint_axes, output_dtype_hints={'result': 'preserve'}):
+            section['result'] = (section[0].data > section[0].data.mean())
+        np.testing.assert_array_equal(
+            processor.outputs['result'].data,
+            self.expected_result.astype(np.uint8),
+        )
+
+    @minimum_python_version(3, 11)
+    @permute_axes('YX', name='joint_axes')
+    def test__yx__preserve__floating(self, joint_axes):
+        for input_dtype in (np.uint8, np.float16):
+            for src_dtype in (np.float32, bool):
+                with self.subTest(input_dtype=input_dtype, src_dtype=src_dtype):
+                    self.images[0].data = self.images[0].data.astype(input_dtype)
+                    processor = giatools.image_processor.ImageProcessor(self.images[0])
+                    for section in processor.process(joint_axes, output_dtype_hints={'result': 'preserve_floating'}):
+                        section['result'] = (section[0].data > section[0].data.mean()).astype(src_dtype)
+                    if src_dtype is np.float32:
+                        expected_dtype = np.float32
+                    elif src_dtype is bool and input_dtype is np.uint8:
+                        expected_dtype = np.float64
+                    elif src_dtype is bool and input_dtype is np.float16:
+                        expected_dtype = np.float16
+                    else:
+                        assert False, (  # sanity check (should not be reached)
+                            f'src_dtype={src_dtype.name}, input_dtype={input_dtype.name}'
+                        )
+                    np.testing.assert_array_equal(
+                        processor.outputs['result'].data,
+                        self.expected_result.astype(expected_dtype),
+                    )
